bc67b69930d4e6314026271aea3a32b1864215ba
sbharath-web <surapanib@gmail.com>
2025-10-16 16:56:35 +0530

    Delete task 3.py

--- task 3.py
+++
@@ -1,9 +0,0 @@
-data = [
-    {'name': 'Raj', 'score': 85, 'age': 21},
-    {'name': 'Anu', 'score': 90, 'age': 20},
-    {'name': 'Ravi', 'score': 85, 'age': 22}
-]
-
-sorted_data = sorted(data, key=lambda x: (-x['score'], x['age']))
-
-print(sorted_data)
